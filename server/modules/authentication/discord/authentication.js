6edd1c8f4393c9e66d4b18d7aaea4bdd27f3c2ee
Philipp Micke <archanyhm@archanyhm.dev>
2025-11-21 21:44:27 +0100

    add role mapping

--- server/modules/authentication/discord/authentication.js
+++ server/modules/authentication/discord/authentication.js
@@ -10,7 +10,7 @@ const _ = require('lodash')
 
 
 module.exports = {
-  init (passport, conf) {
+  init(passport, conf) {
     const discord = new DiscordOauth2()
     passport.use(conf.key,
       new DiscordStrategy({
@@ -39,6 +39,38 @@ module.exports = {
               picture: `https://cdn.discordapp.com/avatars/${profile.id}/${profile.avatar}.png`
             }
           })
+
+          if (conf.mapRoles && conf.guildId) {
+            const memberData = await discord.getGuildMember(accessToken, conf.guildId)
+            const discordRoles = memberData.roles || []
+            
+            try {
+              const roleMappings = JSON.parse(conf.roleMappings || '{}')
+              const currentGroups = (await user.$relatedQuery('groups').select('groups.id')).map(g => g.id)
+              
+              // Build expected groups from role mappings
+              const expectedGroups = []
+              for (const [discordRoleId, groupName] of Object.entries(roleMappings)) {
+                if (discordRoles.includes(discordRoleId)) {
+                  const group = Object.values(WIKI.auth.groups).find(g => g.name === groupName)
+                  if (group) {
+                    expectedGroups.push(group.id)
+                  }
+                }
+              }
+              
+              // Sync groups
+              for (const groupId of _.difference(expectedGroups, currentGroups)) {
+                await user.$relatedQuery('groups').relate(groupId)
+              }
+              for (const groupId of _.difference(currentGroups, expectedGroups)) {
+                await user.$relatedQuery('groups').unrelate().where('groupId', groupId)
+              }
+            } catch (err) {
+              WIKI.logger.warn(`Failed to map Discord roles for user ${user.id}:`, err.message)
+            }
+          }
+
           cb(null, user)
         } catch (err) {
           cb(err, null)
